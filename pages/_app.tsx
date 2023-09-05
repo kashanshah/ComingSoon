b85b6bba014c4fcbb4c390f39214fd94037dfa7c
Kashan Shah <kashan.shah@americaneagle.com>
2023-09-06 03:16:18 +0400

    feat: replacing names and form submissions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
 import "../styles/globals.css";
 import React from "react";
 import type { AppProps } from "next/app";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <ToastContainer />
+    </>
+  );
 }
 
 export default MyApp;
