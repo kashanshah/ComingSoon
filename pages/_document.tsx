2cf2a559e795efaec69b767e017f95b6715a8cc5
Kashan Shah <kashan.shah@americaneagle.com>
2023-09-08 14:13:58 +0400

    feat: favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,15 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com"/>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'} />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'true'}/>
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700;800&display=swap"
               rel="stylesheet"/>
+
+        <link rel="apple-touch-icon" sizes="180x180" href="/images/favicon/apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="/images/favicon/favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="/images/favicon/favicon-16x16.png"/>
+        <link rel="manifest" href="/images/favicon/site.webmanifest"/>
+
       </Head>
       <body>
       <Main/>
